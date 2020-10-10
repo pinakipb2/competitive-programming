0048e1a500a3498eece38911f37e8f04662c8ec7
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-10 20:20:43 +0530

    10 oct 2020

--- Life_the_Universe_and_Everything.cpp
+++ Life_the_Universe_and_Everything.cpp
@@ -1,16 +1,17 @@
 //Code Written by PINAKI BHATTACHARJEE
 #include <bits/stdc++.h>
 #define MAX 1000
+typedef long long ll;
 using namespace std;
 int main()
 {
-    int arr[MAX], i = 0;
+    ll a, i = 0;
     for (i = 0; ; i++)
     {
-        cin >> arr[i];
-        if (arr[i] == 42)
+        cin >> a;
+        if (a == 42)
             break;
-        cout << arr[i] << endl;
+        cout << a << endl;
     }
     return 0;
 }
