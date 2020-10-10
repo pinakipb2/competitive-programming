0d9084a3822b89bec0cf4ff2ae5aa756f332c254
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-10 18:55:10 +0530

    10 oct 2020

--- Lapindromes.cpp
+++ Lapindromes.cpp
@@ -13,41 +13,28 @@ int main()
         int n=str.length();
         string str_a="";
         string str_b="";
-        if(n%2!=0)
+        int k = n%2==0 ? n/2 : n/2+1 ;
+        for(int i=0;i<n/2;i++)
         {
-            for(int i=0;i<n/2;i++)
-            {
-                str_a+=str[i];
-            }
-            for(int i=n/2+1;i<n;i++)
-            {
-                str_b+=str[i];
-            }
+            str_a+=str[i];
         }
-        else
+        for(int i=k;i<n;i++)
         {
-            for(int i=0;i<n/2;i++)
-            {
-                str_a+=str[i];
-            }
-            for(int i=n/2;i<n;i++)
-            {
-                str_b+=str[i];
-            }
+            str_b+=str[i];
         }
         sort(str_a.begin(),str_a.end());
         sort(str_b.begin(),str_b.end());
         int n1=str_a.length();
-        int count=1;
+        int flag=1;
         for(int i=0;i<n1;i++)
         {
             if(str_a[i]!=str_b[i])
             {
-                count=0;
+                flag=0;
                 break;
             }
         }
-        if(count)
+        if(flag)
         {
             cout << "YES" << endl;
         }
