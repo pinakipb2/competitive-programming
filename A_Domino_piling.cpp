a2492184aa820dc7746542bee98c7f3471891e4c
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-12 15:18:58 +0530

    12 oct 2020

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,22 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+//𝓓𝓪𝓽𝓮 : 12-October-2020    15:16:34
+#include<bits/stdc++.h>
+#define MAX 10000
+typedef long long ll;
+using namespace std;
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    int a,b;
+    cin>>a>>b;
+    cout<<(a*b)/2<<endl;
+    return 0;
+}
