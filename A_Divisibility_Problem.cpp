36c8bde63bc16309af42131af4b268d4fcde7a2f
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-13 09:36:51 +0530

    13 oct 2020

---
+++ A_Divisibility_Problem.cpp
@@ -0,0 +1,36 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+//𝓓𝓪𝓽𝓮 : 13-October-2020  09:30:54  (IST)(GMT+5:30)
+#include<bits/stdc++.h>
+#define MAX 10000
+typedef long long ll;
+using namespace std;
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,m;
+        cin>>a>>b;
+        if(a%b==0)
+        {
+            cout<<"0"<<endl;
+        }
+        else
+        {
+            m = a/b;
+            m = b*(m+1);
+            cout<<m-a<<endl;
+        }
+    }
+    return 0;
+}
