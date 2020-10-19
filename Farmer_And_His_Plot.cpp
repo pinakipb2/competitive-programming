9b07792cba6d3e9803eec8e1f1d60e8e02d5af4c
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-19 22:23:06 +0530

    19 oct 2020

---
+++ Farmer_And_His_Plot.cpp
@@ -0,0 +1,30 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,s;
+        cin>>a>>b;
+        s = __gcd(a,b);
+        cout<<(a*b)/(s*s)<<endl;
+    }
+    return 0;
+}
