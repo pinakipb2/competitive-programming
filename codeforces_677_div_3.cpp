be66a8ed8998a557043e4ac37047e29ab5d5f747
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-20 22:33:25 +0530

    20 oct 2020

---
+++ codeforces_677_div_3.cpp
@@ -0,0 +1,42 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x,n=0;
+        cin>>x;
+        ll val = x%10;
+        while(x!=0)
+        {
+            x/=10;
+            n++;
+        }
+        ll ans = (val-1)*10;
+        ll sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        ans+=sum;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
