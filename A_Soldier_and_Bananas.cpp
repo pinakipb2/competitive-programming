b5836a21425fd03ff7cfe019773cf307bf3c74da
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-21 10:38:30 +0530

    21 oct 2020

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,35 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll k,n,w,sum=0;
+    cin>>k>>n>>w;
+    for(ll i=1;i<=w;i++)
+    {
+        sum += i*k;
+    }
+    if(sum-n>=0)
+    {
+        cout<<sum-n<<endl;
+    }
+    else
+    {
+        cout<<0<<endl;
+    }
+    return 0;
+}
