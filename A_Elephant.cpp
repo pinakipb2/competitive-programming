7e0a04021a996fbe5581b9bc103b6daf97ead52d
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-21 10:48:32 +0530

    21 oct 2020

---
+++ A_Elephant.cpp
@@ -0,0 +1,36 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll a,cnt=0;
+    cin>>a;
+    if(a<=5)
+    {
+        cout<<1<<endl;
+    }
+    else
+    {
+        while(a>5)
+        {
+            cnt++;
+            a = a-5;
+        }
+        cout<<cnt+1<<endl;
+    }
+    return 0;
+}
