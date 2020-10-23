35c7d3b8f32b2d7a614c2ee08b087c3a5172d16f
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-23 19:42:17 +0530

    23 oct 2020

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,35 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+
+ll func(ll x)
+{
+    ll ans;
+    if(x%2==0)
+        ans = x/2;
+    else
+        ans = -(x+1)/2;
+    return ans;
+}
+
+int main()
+{
+    pinakipb2();
+    ll n;
+    cin>>n;
+    cout<<func(n)<<endl;
+    return 0;
+}
