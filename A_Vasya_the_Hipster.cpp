f95c1e7a96330edc39d66ee939388435cf203ac8
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-24 09:38:27 +0530

    24 oct 2020

---
+++ A_Vasya_the_Hipster.cpp
@@ -0,0 +1,27 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define MAX 10000
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll a,b;
+    cin>>a>>b;
+    cout<<min(a,b)<<" ";
+    ll c = max(a,b) - min(a,b);
+    c/=2;
+    cout<<c<<endl;
+    return 0;
+}
