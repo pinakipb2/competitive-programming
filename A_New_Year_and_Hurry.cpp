74b22c5e59c87231f645e0c648bf319622e35431
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-25 14:21:10 +0530

    25 oct 2020

---
+++ A_New_Year_and_Hurry.cpp
@@ -0,0 +1,41 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define MAX 10000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+const int MOD = (int) 1e9 + 7;
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    ll n,i,k,cnt=240;
+    cin>>n>>k;
+    cnt-=k;
+    for(i=1;i<=n;i++)
+    {
+        if(cnt<=0 || cnt<5*i)
+            break;
+        else
+        {
+            cnt-=5*i;
+        }
+    }
+    cout<<i-1<<endl;
+    return 0;
+}
