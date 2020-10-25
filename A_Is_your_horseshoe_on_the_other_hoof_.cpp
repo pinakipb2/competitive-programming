02b312a8a9f9113a1a7edef890db872e2e982ea8
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-25 14:41:29 +0530

    25 oct 2020

---
+++ A_Is_your_horseshoe_on_the_other_hoof_.cpp
@@ -0,0 +1,42 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define MAX 10000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+const int MOD = (int) 1e9 + 7;
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int main()
+{
+    pinakipb2();
+    vector<ll> v;
+    ll ele;
+    for(int i=0;i<4;i++)
+    {
+        cin>>ele;
+        v.pb(ele);
+    }
+    ll ans = v.size();
+    sort(all(v));
+    vector<ll>::iterator it;
+    it = unique(all(v));
+    v.resize(distance(v.begin(),it));
+    ans-=v.size();
+    cout<<ans<<endl;
+    return 0;
+}
