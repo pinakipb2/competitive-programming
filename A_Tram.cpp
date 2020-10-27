471ab33db68e53c8e5053f2fc7ee677820914d3e
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-27 20:25:16 +0530

    27 oct 2020

---
+++ A_Tram.cpp
@@ -0,0 +1,43 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define MAX 10000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define DEBUG(x) cerr << #x << " = " << x << endl;
+
+const int MOD = (int) 1e9 + 7;
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+
+int main()
+{
+    pinakipb2();
+    ll t;
+    cin>>t;
+    ll arr[2*t],maxx=0,curr=0;
+    for(int i=0;i<2*t;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<2*t;i+=2)
+    {
+        curr += arr[i+1]-arr[i];
+        maxx = max(maxx,curr);
+    }
+    cout<<maxx<<endl;
+    return 0;
+}
