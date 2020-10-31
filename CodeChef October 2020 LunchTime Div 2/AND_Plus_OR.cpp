2c86444739d327d30228912f099cec7bba872e48
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-10-31 23:07:24 +0530

    31 oct 2020

---
+++ CodeChef October 2020 LunchTime Div 2/AND_Plus_OR.cpp
@@ -0,0 +1,38 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define MAX 10000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define DEBUG(x) cerr << #x << " = " << x << endl;
+
+const int MOD = (int) 1e9 + 7;
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+
+int main()
+{
+    pinakipb2();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin>>x;
+        cout<<1<<" "<<x-1<<endl;
+    }
+    return 0;
+}
