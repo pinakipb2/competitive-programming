33ee108851fe9f2530e4eba0e8d18b3d1117afd7
Pinaki Bhattacharjee <pinakipb2@users.noreply.github.com>
2020-11-02 23:03:45 +0530

    2 nov 2020

---
+++ A_Fingerprints.cpp
@@ -0,0 +1,56 @@
+//𝓒𝓸𝓭𝓮 𝓦𝓻𝓲𝓽𝓽𝓮𝓷 𝓫𝔂 𝓟𝓲𝓷𝓪𝓴𝓲 𝓑𝓱𝓪𝓽𝓽𝓪𝓬𝓱𝓪𝓻𝓳𝓮𝓮
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define MAX 10000
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define DEBUG(x) cerr << #x << " = " << x << endl;
+
+const int MOD = (int) 1e9 + 7;
+
+void pinakipb2()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+
+int main()
+{
+    pinakipb2();
+    ll a,b,cnt=0;
+    cin>>a>>b;
+    ll arr1[a],arr2[b];
+    for(int i=0;i<a;i++)
+    {
+        cin>>arr1[i];
+    }
+    for(int i=0;i<b;i++)
+    {
+        cin>>arr2[i];
+    }
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<b;j++)
+        {
+            if(cnt==b)
+            {
+                break;
+            }
+            if(arr1[i]==arr2[j])
+            {
+                cout<<arr1[i]<<" ";
+                cnt++;
+            }
+        }
+    }
+    return 0;
+}
